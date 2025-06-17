55e532799d62f30a32d72b49b7b1da92b15d4fde
victorbotez92 <93555142+victorbotez92@users.noreply.github.com>
2025-06-17 10:43:06 +0200

    Update read_suite.py

--- read_write_SFEMaNS/read_suite.py
+++ read_write_SFEMaNS/read_suite.py
@@ -181,13 +181,14 @@ def get_data_from_suites(par,I,mF_to_read,record_stack_lenght=7, get_gauss_point
         
         # node point to gauss points
         TEMP_gauss =[]   
-        TEMP = np.array(TEMP)
-        print(TEMP.shape)
-        for s in range(par.S):
-            #arange field by triangle
-            X = np.asarray( [TEMP[:][s][:,mesh_jj[s][i]-1,:] for i in range(n_w)] )
-            field_gauss = einsum(X,mesh_ww[s],'MF nw t me d, nw l_G -> MF t me l_G d ')
-            TEMP_gauss.append( rearrange(field_gauss, "MF t me l_G d -> MF t (me l_G) d") )
+        for mF in range(len(TEMP)):
+            TEMP_gauss_per_mF = []
+            for s in range(par.S):
+                #arange field by triangle
+                X = np.asarray( [TEMP[mF][s][:,mesh_jj[s][i]-1,:] for i in range(n_w)] )
+                field_gauss = einsum(X,mesh_ww[s],'MF nw t me d, nw l_G -> MF t me l_G d ')
+                TEMP_gauss_per_mF.append( rearrange(field_gauss, "MF t me l_G d -> MF t (me l_G) d") )
+            TEMP_gauss.append(TEMP_gauss_per_mF)
         TEMP = TEMP_gauss
     
     if stack_domains:
