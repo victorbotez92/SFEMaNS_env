20d5b6548696d30fc87c04637d499bb4014888dd
victorbotez92 <93555142+victorbotez92@users.noreply.github.com>
2025-06-17 10:24:19 +0200

    Update read_suite.py

--- read_write_SFEMaNS/read_suite.py
+++ read_write_SFEMaNS/read_suite.py
@@ -144,7 +144,7 @@ def get_data_from_suites(par,I,mF_to_read,record_stack_lenght=7, get_gauss_point
     for s in range(par.S):
         suite_names.append(f'{suite_kind}_S{s:03d}{add_str}{par.mesh_ext}')
 
-    suite_files = [par.opt_path_suite+'/'+elm for elm in suite_names]
+    suite_files = [par.path_suites+'/'+elm for elm in suite_names]
 
     Nfile = len(suite_files)
     Nt = Nfile//par.S
